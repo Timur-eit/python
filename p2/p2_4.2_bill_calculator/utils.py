a20abcd208cbcc116e0a5e3a6fd273f6298a8af2
Evgeny <evtsyga@gmail.com>
2025-06-29 15:35:19 +0300

    p2: tests comment

--- p2/p2_4.2_bill_calculator/utils.py
+++ p2/p2_4.2_bill_calculator/utils.py
@@ -11,3 +11,14 @@ def get_formatted_number(value: float, digits = ROUND_NUMBER) -> str:
   Утилита для форматирования строки (по умолчанию 2 знака после запятой)
   """
   return f"{value:,.{digits}f}"
+
+
+# утилиты и бизнес-логика должны быть покрыты тестами – в идеале должны быть авто-тест
+# get_added_value
+# print('get_added_value: 100, 10 -> 10', get_added_value(100, 10) == 10)
+# print('get_added_value: 100, 20 -> 20', get_added_value(100, 20) == 20)
+# print('get_added_value: -100, 20 -> -20', get_added_value(-100, 20) == -20)
+
+# get_formatted_number
+# print('get_formatted_number: 100 -> "100.00"', get_formatted_number(100) == '100.00')
+# print('get_formatted_number: 100 -> "1,000.00"', get_formatted_number(1000) == '1,000.00')
