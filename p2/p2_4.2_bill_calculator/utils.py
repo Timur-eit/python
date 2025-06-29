3305936ed7ddb86c81f056363f4420df6a213bab
Evgeny <evtsyga@gmail.com>
2025-06-29 15:55:45 +0300

    fix type

--- p2/p2_4.2_bill_calculator/utils.py
+++ p2/p2_4.2_bill_calculator/utils.py
@@ -6,7 +6,7 @@ def get_added_value(net_order_value: float, add_percent: float) -> float:
   """
   return (net_order_value * (add_percent / ONE_HUNDRED))
 
-def get_formatted_number(value: float, digits = ROUND_NUMBER) -> str:
+def get_formatted_number(value: float, digits: int = ROUND_NUMBER) -> str:
   """
   Утилита для форматирования строки (по умолчанию 2 знака после запятой)
   """
