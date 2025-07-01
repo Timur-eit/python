c14f4470d557daf425d031226324a1b9df507e7e
Evgeny <evtsyga@gmail.com>
2025-07-01 21:50:29 +0300

    fix get_user_data_list param

--- 1b/app.py
+++ 1b/app.py
@@ -4,7 +4,7 @@ from utils.get_final_formatted_data import get_final_formatted_data
 
 def app() -> None:
   prompt_user_data = input(f'Введите имя и фамилию через пробел: ')
-  first_name, second_name = get_user_data_list(prompt_user_data.title().strip())
+  first_name, second_name = get_user_data_list(prompt_user_data.strip().title())
   login = get_login(first_name, second_name)
 
   print(get_final_formatted_data(first_name, second_name, login))
