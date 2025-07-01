08ec5f8c7f401ad0f0edb99683e6e366f69fa8a1
Evgeny <evtsyga@gmail.com>
2025-07-01 21:47:58 +0300

    fix var naming

--- 1b/app.py
+++ 1b/app.py
@@ -4,10 +4,10 @@ from utils.get_final_formatted_data import get_final_formatted_data
 
 def app() -> None:
   prompt_user_data = input(f'Введите имя и фамилию через пробел: ')
-  name, surname = get_user_data_list(prompt_user_data.title().strip())
-  login = get_login(name, surname)
+  first_name, second_name = get_user_data_list(prompt_user_data.title().strip())
+  login = get_login(first_name, second_name)
 
-  print(get_final_formatted_data(name, surname, login))
+  print(get_final_formatted_data(first_name, second_name, login))
 
 # Project 1_1B
 app()
